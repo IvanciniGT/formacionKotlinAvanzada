f65a68976b0edc712059208a9dda5dc99dcc4e66
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2023-09-06 16:18:48 +0200

    Validaciones

--- src/main/kotlin/PersonaEither2.kt
+++ src/main/kotlin/PersonaEither2.kt
@@ -1,8 +1,7 @@
-import arrow.core.Either
-import arrow.core.left
+import arrow.core.*
 import arrow.core.raise.either
 import arrow.core.raise.ensure
-import arrow.core.right
+import arrow.core.raise.zipOrAccumulate
 
 interface PersonaCreationError2 {
     val message: String
@@ -25,27 +24,31 @@ interface PersonaEither2{
     ) : PersonaEither2
     companion object {
         // INVOKE sobreescribe la llamada a la interfaz, como si fuera un constructor
-        operator fun invoke(nombre: String, edad: Int, email: String, dni: String): Either<PersonaCreationError2,PersonaEither2> = either {
+        //operator fun invoke(nombre: String, edad: Int, email: String, dni: String): Either<NonEmptyList<PersonaCreationError2>,PersonaEither2> = either {
+        operator fun invoke(nombre: String, edad: Int, email: String, dni: String): EitherNel<PersonaCreationError2,PersonaEither2> = either {
                 // val mayorDeEdad: Boolean = edad >= 18
                 // ME aseguro que el nombre tenga caracteres y empiece por mayúscula
-                ensure (nombre.matches(Regex("^[A-Z][a-záéíóúñ]+$"))) {
+            zipOrAccumulate(
+                { ensure (nombre.matches(Regex("^[A-Z][a-záéíóúñ]+$"))) {
                     PersonaCreationError2.IllegalNombreOnPersonaCreation("El nombre debe tener caracteres y empezar por mayúscula")
-                }
+                }},
                 // Me aseguro que la edad sea mayor de 0
-                ensure (edad >= 0) {
+                {ensure (edad >= 0) {
                     PersonaCreationError2.IllegalEdadOnPersonaCreation("La edad debe ser mayor de 0")
-                }
+                }},
                 // Me aseguro que el email sea un email
-                ensure (email.matches(Regex("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"))) {
+                {ensure (email.matches(Regex("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"))) {
                     PersonaCreationError2.IllegalEmailOnPersonaCreation("El email debe ser un email")
-                }
+                }},
                 // Me aseguro que el dni sea un dni
-                ensure (dni.matches(Regex("^[0-9]{1,8}[A-Z]$"))) {
+                {ensure (dni.matches(Regex("^[0-9]{1,8}[A-Z]$"))) {
                     PersonaCreationError2.IllegalDniOnPersonaCreation("El dni debe ser un dni")
-                }
+                }}
+            ) { _,_,_,_ -> // Como parametros de entrada, esta función recibe los resultados de las 4 funciones anteriores de validación
                 // Devuelvo un Either con el valor de la derecha, el bueno. Esto es el caso: TODO HA IDO BIEN
                 //return Either.Right(PersonaEither2Impl(nombre, edad, email, dni))
                 PersonaEither2Impl(nombre, edad, email, dni) // Este dato se envuelve en el RIGHT del EITHER si no hay problemas, IGUAL que al trabajar con RESULTs
+                }
         }
     }
 }
@@ -55,39 +58,25 @@ interface PersonaEither2{
 
 fun main(){
     // Crear una Persona
-    var p1: Either<PersonaCreationError2, PersonaEither2> = PersonaEither2("Pepe", 23, "ivan@pepe.com", "12345678A")
-    p1 = PersonaEither2("felipe", -17, "http://felipe.com", "ABCDEFGH1")
-    if (p1.isRight()) {
-        println("Persona creada correctamente")
-        println(p1.getOrNull()) // Esto devuelve el valor de la derecha
-    }else {
-        println("Error al crear la persona: ${p1.leftOrNull()?.message}")
-        when(p1.leftOrNull()) {
-            is PersonaCreationError2.IllegalNombreOnPersonaCreation -> println("Error al crear la persona. Su nombre es inválido: ${p1.leftOrNull()?.message}")
-            is PersonaCreationError2.IllegalEdadOnPersonaCreation -> println("Error al crear la persona. Su edad es inválida: ${p1.leftOrNull()?.message}")
-            is PersonaCreationError2.IllegalEmailOnPersonaCreation -> println("Error al crear la persona. Su email es inválido: ${p1.leftOrNull()?.message}")
-            is PersonaCreationError2.IllegalDniOnPersonaCreation -> println("Error al crear la persona. Su dni es inválido: ${p1.leftOrNull()?.message}")
-            else -> println("Otro error al crear la persona: ${p1.leftOrNull()?.message}")
-        }
-    }
+    var p1: EitherNel<PersonaCreationError2, PersonaEither2> = PersonaEither2("Pepe", 23, "ivan@pepe.com", "12345678A")
+    p1 = PersonaEither2("Felipe", -17, "http://felipe.com", "ABCDEFGH1")
 
     // Sintaxis alternativa
     p1.fold(
         {
-            when (it) {
-                is PersonaCreationError2.IllegalNombreOnPersonaCreation -> println("Error al crear la persona. Su nombre es inválido: ${it.message}")
-                is PersonaCreationError2.IllegalEdadOnPersonaCreation -> println("Error al crear la persona. Su edad es inválida: ${it.message}")
-                is PersonaCreationError2.IllegalEmailOnPersonaCreation -> println("Error al crear la persona. Su email es inválido: ${it.message}")
-                is PersonaCreationError2.IllegalDniOnPersonaCreation -> println("Error al crear la persona. Su dni es inválido: ${it.message}")
-                else -> println("Otro error al crear la persona: ${it.message}")
+            it.all.forEach { error ->
+                when (error) {
+                    is PersonaCreationError2.IllegalNombreOnPersonaCreation -> println("Error al crear la persona. Su nombre es inválido: ${error.message}")
+                    is PersonaCreationError2.IllegalEdadOnPersonaCreation -> println("Error al crear la persona. Su edad es inválida: ${error.message}")
+                    is PersonaCreationError2.IllegalEmailOnPersonaCreation -> println("Error al crear la persona. Su email es inválido: ${error.message}")
+                    is PersonaCreationError2.IllegalDniOnPersonaCreation -> println("Error al crear la persona. Su dni es inválido: ${error.message}")
+                    else -> println("Otro error al crear la persona: ${error.message}")
+                }
             }
         }
     ){
         println("Persona creada correctamente: ${it.nombre}") // En este caso it, hace referencia al RIGHT
     }
-
-
-
 
 }
 
