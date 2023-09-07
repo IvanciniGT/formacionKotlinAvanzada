4c66bad3df1bb8f6a59c776fe960ff322520848f
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2023-09-07 18:04:34 +0200

    Controller

--- src/main/kotlin/controller/PersonaControllerImpl.kt
+++ src/main/kotlin/controller/PersonaControllerImpl.kt
@@ -1,10 +1,13 @@
 package service
 
 import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import controller.PersonaController
 import controller.dto.DatosNuevaPersonaDTO
 import controller.dto.DatosPersonaDTO
 import controller.error.PersonaControllerError
+import controller.mapper.persona2DatosPersonaDTO
 import model.Persona
 import model.error.RepositorioError
 import repository.PersonaRepository
@@ -19,14 +22,24 @@ constructor (private val personaServicio: PersonaServicio)
         // Me puede dar un error en las validaciones de los datos
         //personaServicio.nuevaPersona(persona)
         TODO("Not yet implemented")
+        // Escribir
     }
 
     override fun recuperarPersona(id: Int): Either<PersonaControllerError, DatosPersonaDTO?> {
         TODO("Not yet implemented")
+        // Escribir
     }
 
     override fun recuperarTodasLasPersonas(): Either<PersonaControllerError, List<DatosPersonaDTO>> {
-        TODO("Not yet implemented")
+        val listado :Either<RepositorioError, List<Persona>> = personaServicio.recuperarTodasLasPersonas()
+        listado.fold({
+            val error = PersonaControllerError()
+            error.errorEnRepositorio = it
+            return error.left()
+        }){
+            return it.map {persona2DatosPersonaDTO(it)}.right()
+        }
+        // Escribir esto con map y flatMap
     }
 
 }
